08eb980f68511bf184e54544af1849bc7423ed19
azuma95 <8ctiagopereira@gmail.com>
2021-04-03 16:32:00 +0100

    commit5 - class PokerRonda

---
+++ TrabalhoPratico/src/trabalhopratico/PokerRonda.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopratico;
+
+/**
+ *
+ * @author 8ctia
+ */
+public class PokerRonda {
+    
+    public void jogadoresOperacoes(){
+    }
+    
+    public void verificaVencedor(){        
+    }
+    
+    public void revelaCarta(){
+    }
+}
