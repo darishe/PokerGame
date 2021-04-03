20c01b035d66c774995558c4365b7692f9909ca8
Tiago Santos <darish26@hotmail.com>
2021-04-03 15:44:13 +0100

    Trabalho Prático LDP, criar repositório

---
+++ TrabalhoPratico/src/trabalhopratico/BaralhoCartas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopratico;
+
+/**
+ *
+ * @author Asus
+ */
+public class BaralhoCartas {
+    Carta[] cartas = new Carta[52] ; 
+    
+    public void baralharCartas(){
+        
+    }
+    public void darCartas(){
+        
+    }
+    
+}
---
+++ TrabalhoPratico/src/trabalhopratico/PokerJogo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopratico;
+
+import java.util.List;
+
+/**
+ *
+ * @author Asus
+ */
+public class PokerJogo {
+    public List<Jogador> jogadores;
+    public BaralhoCartas baralho;
+    
+        public PokerJogo(List<Jogador> jogadores, BaralhoCartas baralho){
+        this.jogadores = jogadores;
+        this.baralho = baralho;
+    }
+    
+    public Jogador bind(){
+        return null;
+    }
+    public void alteraCreditos(){
+        
+    }
+    
+    
+}
